7c20ea7e195b8d35db7b78dc16f12fb4fae935cf
PromytheasN <63561890+PromytheasN@users.noreply.github.com>
2020-05-04 13:21:56 -0700

    Update String-2 > end_othe.py

--- String-2 > end_othe.py
+++ String-2 > end_othe.py
@@ -1,6 +1,5 @@
 def end_other(a, b):
-    a = a.lower()
-    b = b.lower()
+    a,b = a.lower(), b.lower()
     return (b.endswith(a) or a.endswith(b))
     
 #or you can do it return a == b[-(len(b)):] or b == a[-(len(a)):]
