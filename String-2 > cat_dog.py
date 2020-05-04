d5db5f40e9877fd692d63f42b7c2fdc0807c2df6
PromytheasN <63561890+PromytheasN@users.noreply.github.com>
2020-05-04 11:41:48 -0700

    Create String-2 > cat_dog.py

---
+++ String-2 > cat_dog.py
@@ -0,0 +1,11 @@
+def cat_dog(str3):
+    count_cat = 0
+    count_dog = 0
+    for i in range(0,len(str3)):
+        if str3[i:i+3] == "cat":
+            count_cat +=1
+        if str3[i:i+3] == "dog":
+            count_dog += 1
+    if count_dog == count_cat:
+        return True
+    return False
