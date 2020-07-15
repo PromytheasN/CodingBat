fead27c09dc006adf9b6b9bbc17794318c4dd8f9
PromytheasN <63561890+PromytheasN@users.noreply.github.com>
2020-07-15 10:39:20 -0700

    Update String1 All solutions.py

--- String1 All solutions.py
+++ String1 All solutions.py
@@ -7,9 +7,47 @@ def hello_name(name):
 def make_abba(a, b):
   return a+b+b+a
 
-#3)The web is built with HTML strings like "<i>Yay</i>" which draws Yay as italic text. In this example, the "i" tag makes <i> and </i> which surround the word "Yay".
+#3) The web is built with HTML strings like "<i>Yay</i>" which draws Yay as italic text. In this example, the "i" tag makes <i> and </i> which surround the word "Yay".
 #Given tag and word strings, create the HTML string with tags around the word, e.g. "<i>Yay</i>".
 
 def make_tags(tag, word):
   return "<{}>{}</{}>".format(tag,word,tag)
 
+#4) Given an "out" string length 4, such as "<<>>", 
+#and a word, return a new string where the word is in the middle of the out string, e.g. "<<word>>". 
+
+def make_out_word(out, word):
+  return "{}{}{}".format(out[0:2], word, out[2:]) 
+
+#5) Given a string, return a new string made of 3 copies of the last 2 chars of the original string. The string length will be at least 2.
+
+def extra_end(str):
+  return str[len(str)-2:len(str)]*3
+
+#6) Given a string, return the string made of its first two chars, so the String "Hello" yields "He". If the string is shorter than length 2, return whatever there is,
+#so "X" yields "X", and the empty string "" yields the empty string "".
+
+def first_two(str):
+  return str[:2]
+
+#7) Given a string of even length, return the first half. So the string "WooHoo" yields "Woo".
+
+def first_half(str):
+  return str[:len(str)/2]
+
+#8) Given a string, return a version without the first and last char, so "Hello" yields "ell". The string length will be at least 2.
+
+def without_end(str):
+  return str[1:len(str)-1]
+
+#9) Given 2 strings, a and b, return a string of the form short+long+short, with the shorter string on the outside and the longer string on the inside. 
+#The strings will not be the same length, but they may be empty (length 0).
+
+def combo_string(a, b):
+  if len(a) > len(b):
+    return b + a + b
+  else:
+    return a + b + a
+  
+  
+  
