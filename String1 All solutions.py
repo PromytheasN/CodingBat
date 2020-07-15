063276ff6527a6bd6932d7a93bff00d71f9316b8
PromytheasN <63561890+PromytheasN@users.noreply.github.com>
2020-07-15 10:03:59 -0700

    Update String1 All solutions.py

--- String1 All solutions.py
+++ String1 All solutions.py
@@ -3,3 +3,13 @@
 def hello_name(name):
   return "Hello {}!".format(name)
 
+#2) Given two strings, a and b, return the result of putting them together in the order abba, e.g. "Hi" and "Bye" returns "HiByeByeHi".
+def make_abba(a, b):
+  return a+b+b+a
+
+#3)The web is built with HTML strings like "<i>Yay</i>" which draws Yay as italic text. In this example, the "i" tag makes <i> and </i> which surround the word "Yay".
+#Given tag and word strings, create the HTML string with tags around the word, e.g. "<i>Yay</i>".
+
+def make_tags(tag, word):
+  return "<{}>{}</{}>".format(tag,word,tag)
+
